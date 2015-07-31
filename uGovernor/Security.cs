970b8c7ad11b7a513421d1cc8af6fdb364c42768
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-07-31 18:37:01 +0200

    Improved Security

--- uGovernor/Security.cs
+++ uGovernor/Security.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Management;
 using System.Security.Cryptography;
@@ -9,55 +10,114 @@ namespace uGovernor
 {
     static class Security
     {
-        const string AesIV256 = @"!QAZ2WSX#EDC4RFV";
         const int _iterations = 1675;
-        static byte[] pepper = { 70, 85, 51, 94, 6, 15, 41, 31 };
 
         internal static string Encrypt(string text)
         {
+            var salt = CreateSalt();
+
             byte[] src = Encoding.Unicode.GetBytes(text);
 
-            using (var aes = CreateProvider())
-            using (ICryptoTransform encrypt = aes.CreateEncryptor())
+            using (var aes = new AesCryptoServiceProvider())
             {
-                byte[] dest = encrypt.TransformFinalBlock(src, 0, src.Length);
-                return Convert.ToBase64String(dest);
+                aes.BlockSize = 128;
+                aes.KeySize = 256;
+                var key = new Rfc2898DeriveBytes(_fingerPrint.Value, salt, _iterations);
+
+                aes.Key = key.GetBytes(16);
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                aes.GenerateIV();
+                var prefix = Encoding.ASCII.GetBytes($"{salt.Length}_{aes.IV.Length}_");
+
+                using (var memStream = new MemoryStream())
+                {
+                    var underscore = new byte[] { 95 };
+
+                    var bytes = BitConverter.GetBytes(salt.Length);
+                    memStream.Write(bytes, 0, bytes.Length);
+
+                    memStream.Write(underscore, 0, underscore.Length);
+
+                    bytes = BitConverter.GetBytes(aes.IV.Length);
+                    memStream.Write(bytes, 0, bytes.Length);
+
+                    memStream.Write(underscore, 0, underscore.Length);
+
+                    memStream.Write(salt, 0, salt.Length);
+                    memStream.Write(aes.IV, 0, aes.IV.Length);
+
+                    using (ICryptoTransform encrypt = aes.CreateEncryptor())
+                    using (var cryptoStream = new CryptoStream(memStream, encrypt, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(src, 0, src.Length);
+
+                        cryptoStream.FlushFinalBlock();
+
+                        var encrypted = memStream.ToArray();
+                        return Convert.ToBase64String(encrypted);
+                    }
+                }
             }
         }
 
         internal static string Decrypt(string text)
         {
-            byte[] src = Convert.FromBase64String(text);
+            const int underscore = '_';
 
-            using (var aes = CreateProvider())
-            using (ICryptoTransform decrypt = aes.CreateDecryptor())
+            var src = Convert.FromBase64String(text);
+            var saltArray = src.TakeWhile(x => x != underscore).ToArray();
+            var saltLength = BitConverter.ToInt32(saltArray, 0);
+
+            var leftover = src.Skip(saltArray.Length + 1).ToArray();
+            var ivArray = leftover.TakeWhile(x => x != underscore).ToArray();
+            var ivLength = BitConverter.ToInt32(ivArray, 0);
+
+
+            leftover = leftover.Skip(ivArray.Length + 1).ToArray();
+            var salt = leftover.Take(saltLength).ToArray();
+
+            leftover = leftover.ToArray();
+            var iv = leftover.Skip(saltLength).Take(ivLength).ToArray();
+
+
+            var content = src.Skip(saltLength + ivLength + 2 + saltArray.Length + ivArray.Length).ToArray();
+
+
+            using (var aes = new AesCryptoServiceProvider())
             {
-                byte[] dest = decrypt.TransformFinalBlock(src, 0, src.Length);
-                return Encoding.Unicode.GetString(dest);
+                aes.BlockSize = 128;
+                aes.KeySize = 256;
+                var key = new Rfc2898DeriveBytes(_fingerPrint.Value, salt, _iterations);
+
+                aes.Key = key.GetBytes(16);
+                aes.IV = iv;
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                using (ICryptoTransform decrypt = aes.CreateDecryptor())
+                {
+                    byte[] dest = decrypt.TransformFinalBlock(content, 0, content.Length);
+                    return Encoding.Unicode.GetString(dest);
+                }
             }
         }
 
-        static AesCryptoServiceProvider CreateProvider()
+
+
+
+        
+        static byte[] CreateSalt(int size = 8)
         {
-            var aes = new AesCryptoServiceProvider();
-            aes.BlockSize = 128;
-            aes.KeySize = 256;
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                byte[] salt = new byte[size];
+                rng.GetBytes(salt);
 
-            aes.IV = Encoding.UTF8.GetBytes(AesIV256);
-
-            var key = new Rfc2898DeriveBytes(_fingerPrint.Value, pepper, _iterations);
-
-            aes.Key = key.GetBytes(16);
-            aes.Mode = CipherMode.CBC;
-            aes.Padding = PaddingMode.PKCS7;
-
-            return aes;
+                return salt;
+            }
         }
-
-
-
-
-
 
         static Lazy<string> _fingerPrint = new Lazy<string>(() => 
         {
